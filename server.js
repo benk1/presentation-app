7b0645c9d2d4d1893b2d607096138c46f4f69a4e
benk1 <kakengi_bernard@hotmail.com>
2019-04-23 23:55:17 +0300

    fixing route2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //app.use('/',require('./server/routes/api/presenters'));
 
-app.use("/", presenterRouter);
+app.use("/presenters", presenterRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use( express.static(path.join(__dirname, "client/build")));
